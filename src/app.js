44ea4ab8019562ca55f26859c331f69dc4ad553b
Oliver Owen <oliverorangee@gmail.com>
2023-05-15 19:57:33 +0100

    finished strings left query param

--- src/app.js
+++ src/app.js
@@ -1,5 +1,39 @@
 const express = require('express');
+const {
+  sayHello,
+  uppercase,
+  lowercase,
+  countCharacters,
+  firstCharacter,
+  firstCharacters,
+} = require('../lib/strings');
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/strings/hello/:string', (req, res) => {
+  res.status(200).json({ result: sayHello(req.params.string) });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.status(200).json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/count-characters/:string', (req, res) => {
+  res.json({ result: countCharacters(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  res.json({ result: firstCharacter(req.params.string) });
+});
+
+app.get('/strings/hello/:string', (req, res) => {
+  res.json({ result: firstCharacters(req.params.string) });
+});
 
 module.exports = app;
